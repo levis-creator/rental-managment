aa00f28d984180b99e85cc5774e1f67db9b00cd0
levis-creator <47003569+levis-creator@users.noreply.github.com>
2025-03-10 15:05:30 +0300

    eslint fix

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -2,10 +2,10 @@ import { URL } from '@/constants/ApiUrls';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  const { email, password} = await req.json();
+  const { email, password } = await req.json();
 
   try {
-    console.log(email, password)
+    console.log(email, password);
     const res = await fetch(`${URL}register`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -17,7 +17,10 @@ export async function POST(req: Request) {
     }
 
     return NextResponse.json({ message: 'Signup successful' });
-  } catch (error) {
+   
+  } 
+   catch (error: unknown) {
+  // @ts-expect-error: Ignoring TypeScript error for unknown error type
     return NextResponse.json({ message: error.message }, { status: 400 });
   }
 }
